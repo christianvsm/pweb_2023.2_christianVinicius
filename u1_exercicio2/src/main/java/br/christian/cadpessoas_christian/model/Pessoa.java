99f4812c01d823485ec94662105fa23c58a30a6d
Christian Vinicius Soares de Macêdo <101632287+christianvsm@users.noreply.github.com>
2024-02-14 19:52:37 -0300

    começando pessoa.java

--- u1_exercicio2/src/main/java/br/christian/cadpessoas_christian/model/Pessoa.java
+++ u1_exercicio2/src/main/java/br/christian/cadpessoas_christian/model/Pessoa.java
@@ -1,5 +1,32 @@
 package br.christian.cadpessoas_christian.model;
 
-public class Pessoa {
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "pessoas")
+public class Pessoa implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    private String nome;
+    private String email;
+    private String genero;
+    
+    @Collumn(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dataNascimento;
+    
+
 }
