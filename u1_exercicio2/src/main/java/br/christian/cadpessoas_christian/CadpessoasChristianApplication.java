38e8df18fe1d4c48b9a9729fcb82b32c9ccbc741
Christian Vinicius Soares de Macêdo <101632287+christianvsm@users.noreply.github.com>
2024-02-14 18:12:18 -0300

    Spring Initializr

---
+++ u1_exercicio2/src/main/java/br/christian/cadpessoas_christian/CadpessoasChristianApplication.java
@@ -0,0 +1,13 @@
+package br.christian.cadpessoas_christian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasChristianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasChristianApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio2/src/test/java/br/christian/cadpessoas_christian/CadpessoasChristianApplicationTests.java
@@ -0,0 +1,13 @@
+package br.christian.cadpessoas_christian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasChristianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
